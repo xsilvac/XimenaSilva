c78b335d4438aab5633d06c6d9f028a5de5853d8
ximena soledad silva cotrina <ximena.silva@wargostec.com>
2023-03-08 23:24:54 -0500

    component card responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Card from "../components/atoms/Card";
+
+const HomeContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 8%;
+  flex-wrap: wrap;
+  gap: 3.5rem;
+}
+`;
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <HomeContainer>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </HomeContainer>
+  );
+};
 
-export default Home
+export default Home;
